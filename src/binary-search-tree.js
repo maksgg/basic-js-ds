7813b9828d0c2e784cdfac6b777b2481abd1aa09
maksgg <maks.zhovmir.97@gmail.com>
2023-12-25 11:04:31 +0200

    fix: fixed code issue

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,144 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+// Implement the BinarySearchTree class
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // Method to insert a node into the BST
+  add(data) {
+    this.rootNode = this._insert(this.rootNode, data);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _insert(node, data) {
+    if (node === null) {
+      return new Node(data);
+    }
+
+    if (data < node.data) {
+      node.left = this._insert(node.left, data);
+    } else if (data > node.data) {
+      node.right = this._insert(node.right, data);
+    }
+
+    return node;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // Method to check if a value exists in the BST
+  has(data) {
+    return this._search(this.rootNode, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _search(node, data) {
+    if (node === null) {
+      return false;
+    }
+
+    if (data === node.data) {
+      return true;
+    } else if (data < node.data) {
+      return this._search(node.left, data);
+    } else {
+      return this._search(node.right, data);
+    }
   }
 
+  // Method to find a node with a specific value in the BST
+  find(data) {
+    return this._find(this.rootNode, data);
+  }
+
+  _find(node, data) {
+    if (node === null || data === node.data) {
+      return node;
+    }
+
+    if (data < node.data) {
+      return this._find(node.left, data);
+    } else {
+      return this._find(node.right, data);
+    }
+  }
+
+  // Method to remove a node with a specific value from the BST
+  remove(data) {
+    this.rootNode = this._remove(this.rootNode, data);
+  }
+
+  _remove(node, data) {
+    if (node === null) {
+      return null;
+    }
+
+    if (data < node.data) {
+      node.left = this._remove(node.left, data);
+    } else if (data > node.data) {
+      node.right = this._remove(node.right, data);
+    } else {
+      // Node with only one child or no child
+      if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      }
+
+      // Node with two children
+      node.data = this._getMinValue(node.right);
+      node.right = this._remove(node.right, node.data);
+    }
+
+    return node;
+  }
+
+  _getMinValue(node) {
+    let minValue = node.data;
+    while (node.left !== null) {
+      minValue = node.left.data;
+      node = node.left;
+    }
+    return minValue;
+  }
+
+  // Method to find the minimum value in the BST
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode) {
+      return null;
+    }
+
+    let currentNode = this.rootNode;
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+
+    return currentNode.data;
   }
 
+  // Method to find the maximum value in the BST
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode) {
+      return null;
+    }
+
+    let currentNode = this.rootNode;
+    while (currentNode.right !== null) {
+      currentNode = currentNode.right;
+    }
+
+    return currentNode.data;
   }
 }
 
--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+
+class ListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
 class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
+  // Method to get the underlying linked list
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  // Method to add an element to the queue
+  enqueue(value) {
+    const newNode = new ListNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   }
 
+  // Method to remove and return the top element from the queue
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return null;
+    }
+
+    const removedValue = this.head.value;
+    this.head = this.head.next;
+
+    if (!this.head) {
+      this.tail = null;
+    }
+
+    return removedValue;
   }
 }
 
